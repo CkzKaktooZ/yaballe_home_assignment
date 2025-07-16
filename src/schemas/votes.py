9c5acb429cb5eeb489433cd08dea2649fd074c60
CkzKaktooZ <max.leshkov5@gmail.com>
2025-07-17 00:48:54 +0300

    deleted unused schema

--- src/schemas/votes.py
+++ src/schemas/votes.py
@@ -5,13 +5,6 @@ class VoteTypeEnum(str, Enum):
     upvote = "upvote"
     downvote = "downvote"
 
-
-class VoteBase(BaseModel):
-    post_id: int
-    user_id: int
-    vote_type: VoteTypeEnum
-
-
 class VoteCount(BaseModel):
     upvotes: int
     downvotes: int
