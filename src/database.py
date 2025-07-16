08830ac341236b9970be551cb911beadb2ebf6a6
CkzKaktooZ <max.leshkov5@gmail.com>
2025-07-16 14:44:19 +0300

    local database stuff

--- src/database.py
+++ src/database.py
@@ -1,18 +1,16 @@
+# app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+DATABASE_URL = "sqlite:///./db.sqlite3"  # Database file will be created in the current directory
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
